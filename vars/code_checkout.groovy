6768620c387219b37179083fc9ea5531ca6a972f
Shashank Parashar <52383423+shashank0490@users.noreply.github.com>
2025-02-23 13:07:23 +0530

    Update code_checkout.groovy

--- vars/code_checkout.groovy
+++ vars/code_checkout.groovy
@@ -1,3 +1,3 @@
 def call(String GitUrl, String GitBranch){
-  checkout scmGit(branches: [[name: '*/${GitBranch}']], extensions: [], userRemoteConfigs: [[url: "${GitUrl}"]])
+  checkout scmGit(branches: [[name: "*/${GitBranch}"]], extensions: [], userRemoteConfigs: [[url: "${GitUrl}"]])
 }
